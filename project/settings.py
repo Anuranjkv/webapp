b435a82b1bf283cb184547ae66657aaec50f40fb
Anuranj <anuranj.git@gmail.com>
2026-03-28 22:31:49 +0530

    register and main

--- project/settings.py
+++ project/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login',
+    'register',
 ]
 
 # Middleware
--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from login import views as login
+from register import views as register
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login.student_login, name='student_login'),
     path('', login.student_login, name='student_login'),
+    path('register/', register.register, name='register_user'),
 ]
